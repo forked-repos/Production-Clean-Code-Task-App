98d65b7cd69766f8cb0b939b617427db6a25f4fd
JamieCorkhill <jamie.corkhill@Live.com>
2020-02-04 17:48:42 -0600

    Add validation

---
+++ src/features/users/validation/userValidation.ts
@@ -0,0 +1,64 @@
+import Joi from '@hapi/joi';
+
+const userCommonFields: { [key: string]: Joi.AnySchema } = {
+    firstName: Joi.string()
+        .min(2)
+        .max(20)
+        .required(),
+
+    lastName: Joi.string()
+        .min(2)
+        .max(30)
+        .required(),
+
+    username: Joi.string()
+        .min(2)
+        .max(20)
+        .required(),
+
+    biography: Joi.string()
+        .min(2)
+        .max(350)
+        .required(),
+
+    email: Joi.string()
+        .email()
+        .required(),
+};
+
+const joiGenericMapper = (
+    toModify: Joi.SchemaMap, 
+    modifier: (currSchema: Joi.AnySchema) => Joi.AnySchema
+): Joi.SchemaMap => {
+    const modifiedObject: Joi.SchemaMap = {};
+
+    Object.keys(toModify).map((key) => {
+        modifiedObject[key] = modifier(userCommonFields[key]);
+    });
+
+    return modifiedObject;
+};
+
+export namespace UserValidators {
+    export const createUser = Joi.object().keys({
+        ...userCommonFields,
+    
+        password: Joi.string()
+            .regex(/[A-Z]/, { name: 'Password must include an uppercase letter.' })
+            .regex(/[a-z]/, { name: 'Password must include a lowercase letter.' })
+            .regex(/\d/, { name: 'Password must include a number.' })
+            .regex(/\W/, { name: 'Password must include special character.' })
+            .required()
+    });  
+
+    export const updateUser = Joi.object(joiGenericMapper(userCommonFields, currSchema => currSchema.optional()));
+
+    export const user = createUser.keys({
+        id: Joi.string().required()
+    });
+
+    export const userCredentials = Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().required()
+    });
+}
---
+++ src/utils/wrappers/joi/joiWrapper.ts
@@ -0,0 +1,20 @@
+import { ObjectSchema, ValidationError } from '@hapi/joi';
+
+import { Either, left, right } from './../../logic/Either';
+
+type TErrorMessage = string;
+
+/**
+ * Validates a given candidate against its schema, returning an Either Monad result.
+ * @param schema    A Joi Schema to validate the `candidate` against.
+ * @param candidate A candidate object to validate against the `schema`.
+ */
+export const validate = <TCandidate>(schema: ObjectSchema, candidate: TCandidate): Either<TErrorMessage, TCandidate> => {
+    const { error, value } = schema.validate(candidate);
+
+    if (error) {
+        return left((error as ValidationError).message);
+    } else {
+        return right(value);
+    }
+}
