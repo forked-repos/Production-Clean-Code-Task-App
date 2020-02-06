554fe8eda11d2ffbdba78ec44e59d2f337660575
JamieCorkhill <jamie.corkhill@Live.com>
2020-02-05 22:45:09 -0600

    Refactor Domain-Egress DTO Mappers

--- src/features/users/mappers/domain-egress-dto/mappers.ts
+++ src/features/users/mappers/domain-egress-dto/mappers.ts
@@ -1,18 +1,26 @@
 import { User } from './../../models/domain/userDomain';
 
 import UserResponseDTO from './../../dtos/egress/userResponseDTO';
+import UserCollectionResponseDTO from './../../dtos/egress/userCollectionResponseDTO';
 
-export const mappers = {
-    toUserResponseDTO(user: User): UserResponseDTO {
-        return {
-            user: {
-                id: user.id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                username: user.username,
-                email: user.email,
-                biography: user.biography
-            }
-        };
-    }
+export interface IDomainEgressDTOMapper {
+    toUserResponseDTO(user: User): UserResponseDTO;
+    toUserCollectionResponseDTO(users: User[]): UserCollectionResponseDTO;
+}
+
+export const mappers: IDomainEgressDTOMapper = {
+    toUserResponseDTO: (user: User): UserResponseDTO => ({
+        user: {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username,
+            email: user.email,
+            biography: user.biography
+        }
+    }),
+
+    toUserCollectionResponseDTO: (users: User[]): UserCollectionResponseDTO => ({
+        users: users.map(user => user) 
+    })
 }
