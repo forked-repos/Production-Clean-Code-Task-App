73cd53a267a4bf4047a63b96953586a86ab6c278
JamieCorkhill <jamie.corkhill@Live.com>
2020-02-07 16:04:12 -0600

    Add tests on Fakes

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
     ],
     "testMatch": [
         "**/__tests__/**/*.+(ts|tsx|js)",
-        "**/?(*.)+(spec|test).+(ts|tsx|js)"
+        "**/?(*.)+(spec|test).+(ts|tsx|js)",
+        "**/?(*.)+(spec|test-attempt).+(ts|tsx|js)"
     ],
     "transform": {
         "^.+\\.(ts|tsx)$": "ts-jest"
