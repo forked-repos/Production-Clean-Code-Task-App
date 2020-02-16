840773158f9d5d53d14d789c17437942f10adf77
JamieCorkhill <jamie.corkhill@Live.com>
2020-02-16 17:46:15 -0600

    Create User Signed Up Handler for Bus

--- src/features/users/observers/onUserSignedUp.ts
+++ src/features/users/observers/onUserSignedUp.ts
@@ -1,42 +1,42 @@
-import { UserEventingChannel, UserSignedUpEvent } from './events';
+import { UserSignedUpEvent } from './events';
 import { EventBuses } from '../../../loaders/loadBuses';
 import { IEventHandler } from '../../../common/buses/EventBus';
 
 const busMaster = EventBuses.masterEventBus;
 const userEventBus = busMaster.getBus('userEventBus');
 
-class EnqueueWelcomeEmailHandler implements IEventHandler<UserSignedUpEvent> {
-    handleEvent(event: UserSignedUpEvent): void {
-        throw new Error("Method not implemented.");
+// Fake - Will move later.
+interface IEmailService {
+    sendEmail(): void;
+}
+interface ISlackNotificationService {
+    pushSlackNotification(): void;
+}
+
+interface IGoogleAnalyticsService {
+    addEvent(): void;
+}
+
+interface IQueueRepository {
+    push(name: string, data: any): void;
+}
+
+// Decorator?
+export class UserSignedUpHandler implements IEventHandler<UserSignedUpEvent> {
+    public constructor (
+        private slackNotificationService: ISlackNotificationService,
+        private gaService: IGoogleAnalyticsService,
+        private queueRepository: IQueueRepository
+    ) {}
+
+    async handleEvent(event: UserSignedUpEvent) {
+        // Job Queueing
+        await this.queueRepository.push('welcome-email', event);
+        await this.queueRepository.push('avatar-image-upload', event);
+        
+        // Other Misc
+        await this.slackNotificationService.pushSlackNotification();
+        await this.gaService.addEvent();
     }
 }
-
-class EnqueueImageProcessHandler implements IEventHandler<UserSignedUpEvent> {
-    handleEvent(event: UserSignedUpEvent): void {
-        throw new Error("Method not implemented.");
-    }
-}
-
-const userSignedUpHandlersProvider = () => {
-    const enqueueWelcomeEmail = userEventBus.subscribe(UserEventingChannel.USER_SIGNED_UP, payload => {
-        console.log(`Sent welcome email to address: ${payload.email}.`);
-    });
-    
-    // Push job to queue to process and upload images.
-    const enqueueImageProcessing = userEventBus.subscribe(UserEventingChannel.USER_SIGNED_UP, payload => {
-        console.log(`Enqueued image processing job for id: ${payload.id}`);
-    });
-    
-    // Push job to queue to send Slack Notification to company channel.
-    const sendSlackNotification = userEventBus.subscribe(UserEventingChannel.USER_SIGNED_UP, payload => {
-        console.log(`Sent Slack Notification for new sign up. User had id ${payload.id}`);
-    });
-    
-    // Publish Google Analytics event.
-    const pushGAEvent = userEventBus.subscribe(UserEventingChannel.USER_SIGNED_UP, payload => {
-        console.log(`Investors are happy. Pushed Google Analytics event. ${payload.id}`)
-    });
-}
-
-
 
