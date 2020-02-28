ca3672c4e6e677a376f7683cad9925b60853edaf
Jamie Corkhill <jamie.corkhill@live.com>
2020-02-28 14:51:14 -0600

    Add Outbox Factory tests

---
+++ src/common/outbox/outbox.test.ts
@@ -0,0 +1,34 @@
+import { outboxFactory } from "./outbox";
+
+test('should create an outbox message with the provided id', () => {
+    // Arrange
+    const domain = 'Users';
+    const id = 'id';
+    const payload = { anything: 'some-string' };
+
+    // Act
+    const outboxMessage = outboxFactory(domain, payload, id);
+
+    // Assert
+    expect(outboxMessage).toEqual({
+        outbox_id: id,
+        domain,
+        payload: JSON.stringify(payload)
+    });
+});
+
+test('should generate id automatically', () => {
+    // Arrange
+    const domain = 'Users';
+    const payload = { anything: 'some-string' };
+
+    // Act
+    const outboxMessage = outboxFactory(domain, payload);
+
+    // Assert
+    expect(outboxMessage).toEqual({
+        outbox_id: expect.any(String),
+        domain,
+        payload: JSON.stringify(payload)
+    });
+});
